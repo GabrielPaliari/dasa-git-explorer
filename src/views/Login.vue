b6375b8a4d98582130a9d2badc382f969525a886
Gabriel Paliari Zanoni <gabriel.paliari@gmail.com>
2021-02-06 20:13:32 -0300

    feat(login-page): change styles and logo

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,53 +1,24 @@
 <template>
   <div class="wrapper">
     <div class="section page-header header-filter" :style="headerStyle">
+      <div class="background-filter-overlay"></div>
       <div class="container">
         <div class="md-layout">
           <div
             class="md-layout-item md-size-33 md-small-size-66 md-xsmall-size-100 md-medium-size-40 mx-auto"
           >
-            <login-card header-color="green">
-              <h4 slot="title" class="card-title">Login</h4>
+            <login-card header-color="primary">
+              <h4 slot="title" class="card-title">Entre com o Github:</h4>
               <md-button
                 slot="buttons"
                 href="javascript:void(0)"
                 class="md-just-icon md-simple md-white"
               >
-                <i class="fab fa-facebook-square"></i>
+                <i class="fab fa-github"></i>
               </md-button>
-              <md-button
-                slot="buttons"
-                href="javascript:void(0)"
-                class="md-just-icon md-simple md-white"
-              >
-                <i class="fab fa-twitter"></i>
-              </md-button>
-              <md-button
-                slot="buttons"
-                href="javascript:void(0)"
-                class="md-just-icon md-simple md-white"
-              >
-                <i class="fab fa-google-plus-g"></i>
-              </md-button>
-              <p slot="description" class="description">Or Be Classical</p>
-              <md-field class="md-form-group" slot="inputs">
-                <md-icon>face</md-icon>
-                <label>First Name...</label>
-                <md-input v-model="firstname"></md-input>
-              </md-field>
-              <md-field class="md-form-group" slot="inputs">
-                <md-icon>email</md-icon>
-                <label>Email...</label>
-                <md-input v-model="email" type="email"></md-input>
-              </md-field>
-              <md-field class="md-form-group" slot="inputs">
-                <md-icon>lock_outline</md-icon>
-                <label>Password...</label>
-                <md-input v-model="password"></md-input>
-              </md-field>
-              <md-button slot="footer" class="md-simple md-success md-lg">
-                Get Started
-              </md-button>
+              <p slot="description" class="description">
+                Faça o Login com o github para começar a explorar os repositórios
+              </p>
             </login-card>
           </div>
         </div>
@@ -74,7 +45,7 @@ export default {
   props: {
     header: {
       type: String,
-      default: require("@/assets/img/profile_city.jpg")
+      default: require("@/assets/img/medical-programmer.jpg")
     }
   },
   computed: {
@@ -87,4 +58,16 @@ export default {
 };
 </script>
 
-<style lang="css"></style>
+<style lang="css">
+  .wrapper {
+    height: 100vh;
+  }
+  .background-filter-overlay {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: #182792b3;
+  }
+</style>
